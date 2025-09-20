4d12943f668701cb28b6c66e7fc3c69636b7f58f
bennyberg <73042035+bennyberg@users.noreply.github.com>
2025-09-20 10:39:39 +0300

    finished JS ex7

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,4 +1,15 @@
-const reverseString = function() {
+const reverseString = function(str) {
+    strLength = str.length;
+    let rev = "";
+    for (let i=str.length - 1; i >= 0; i--)
+    {
+        rev += str[i];
+    }
+
+    return rev;
+
+
+
 
 };
 
