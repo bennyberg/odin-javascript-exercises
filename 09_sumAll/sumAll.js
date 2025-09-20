b001622dcf8126d1f755e3e6d2fba773eef73559
bennyberg <73042035+bennyberg@users.noreply.github.com>
2025-09-20 14:19:26 +0300

    finished JS ex9

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,6 +1,44 @@
-const sumAll = function() {
+const sumAll = function () {
+    const argsArr = Array.from(arguments);
 
-};
+    if (argsArr.length != 2) {
+        return 'ERROR';
+    }
+
+    for (let i = 0; i < argsArr.length; i++) {
+        if (typeof (argsArr[i]) != 'number') {
+            return 'ERROR';
+        }
+
+        if (!Number.isInteger(argsArr[i]) || argsArr[i] < 0)
+        {
+           return 'ERROR'; 
+        }
+    }
+
+    const firstArg = argsArr[0];
+    const secondArg = argsArr[1];
+    const difference = secondArg - firstArg;
+
+    if (difference === 0) {
+        return firstArg;
+    }
+
+    let sum = 0;
+
+    if (firstArg > secondArg) {
+        for (let i = secondArg; i < firstArg + 1; i++) {
+            sum += i;
+        }
+    }
+    else {
+        for (let i = firstArg; i < secondArg + 1; i++) {
+            sum += i;
+        }
+    }
+    console.log(sum);
+    return sum;
+}
 
 // Do not edit below this line
 module.exports = sumAll;
