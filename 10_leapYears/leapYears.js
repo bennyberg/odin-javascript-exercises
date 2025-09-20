dfef3445ea39a1a0d94fd2193e74ea8969b0a6ee
bennyberg <73042035+bennyberg@users.noreply.github.com>
2025-09-20 16:38:48 +0300

    finished JS ex10

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,4 +1,16 @@
 const leapYears = function() {
+    const year = (Array.from(arguments))[0];
+    if (year % 100 === 0){
+        if (year % 400 === 0){
+            return true;
+        }
+        return false;
+    }
+    
+    if (year % 4 === 0 ){
+        return true;
+    }
+    return false;
 
 };
 
