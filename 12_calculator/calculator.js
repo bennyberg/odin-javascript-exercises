90e5f8d219abafcc768f8307571333153f97fb64
bennyberg <73042035+bennyberg@users.noreply.github.com>
2025-09-21 10:19:18 +0300

    finished JS ex12

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,50 @@
-const add = function() {
+const add = function(x,y) {
+  return x+y;
 	
 };
 
-const subtract = function() {
+const subtract = function(x,y) {
+  return x-y;
 	
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  let res = 0;
+  for (const item of arr){
+    res += item;
+  }
+  return res;
 };
 
-const multiply = function() {
+const multiply = function(arr) {
+  let res = 1;
+
+  for (const item of arr)
+  {
+    res *= item;
+  }
+
+  return res;
+
+
 
 };
 
-const power = function() {
-	
+const power = function(x,y) {
+	return x**y;
 };
 
-const factorial = function() {
-	
+const factorial = function(x) {
+  if (x === 0 || x === 1) {
+    return 1;
+  } 
+  else if (x < 0) {
+    return "Factorial is not defined for negative numbers.";
+  }
+  // Recursive step: x * factorial(x - 1)
+  else {
+    return x * factorial(x - 1);
+  }
 };
 
 // Do not edit below this line
