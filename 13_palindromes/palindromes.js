4b07cef52516d4bfe98045a58208d970fcb0bdf6
bennyberg <73042035+bennyberg@users.noreply.github.com>
2025-09-21 10:41:33 +0300

    finished JS ex13

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,4 +1,19 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    let modifiedStr = str.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()\s]/g,"");
+    console.log(modifiedStr);
+    modifiedStr = modifiedStr.toLowerCase();
+    const iterations = Math.round(modifiedStr.length / 2) - 1
+    
+    const strLength = modifiedStr.length;
+
+    for (let i=0; i < iterations;i++)
+    {
+        if (modifiedStr[i] != modifiedStr[strLength - i - 1]){
+            return false;
+        }
+    }
+    return true;
+
 
 };
 
