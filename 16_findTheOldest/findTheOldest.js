e50694165d6ad4e31a03a37c25ca602d66943b23
bennyberg <73042035+bennyberg@users.noreply.github.com>
2025-09-21 12:45:44 +0300

    finished JS ex16

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,4 +1,25 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+    // arr.sort((a, b) => a.yearOfBirth - b.yearOfBirth);
+    // console.log((arr[0])["name"]);
+
+    let modifiedObjects = [];
+    for (const obj of arr){
+        let finalYear = new Date().getFullYear()  
+        if ("yearOfDeath" in obj){
+            finalYear = obj.yearOfDeath;
+        }
+        
+        const newObj = {...obj, age:finalYear - obj.yearOfBirth};
+        modifiedObjects.push(newObj);
+    }
+
+    modifiedObjects.sort((a, b) => b.age - a.age);
+    // console.log(modifiedObjects[0]["name"]);
+
+
+    return modifiedObjects[0];
+
+    
 
 };
 
